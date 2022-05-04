025a0f6a0bfad8de8aa0675334f2226e6b1869b0
Arturo Velasco <ing.ricardo.97@gmail.com>
2022-05-04 10:56:53 -0500

    Archivos al 5 de mayo

---
+++ flujoDeControl/assets/js/flujoDeControl.js
@@ -0,0 +1,43 @@
+opcion = prompt ("Ingresa una opción del 1 al 4");
+
+switch (opcion){
+    case "1":
+        console.log("Seleccionaste la opcion 1");
+        break;
+    case "2":
+        console.log("Seleccionaste la opcion 2");
+        break;
+    case "3":
+        console.log("Seleccionaste la opcion 3");
+        break;
+    case "4":
+        console.log("Seleccionaste la opcion 4");
+        break;
+    default:
+        console.log("acaso me vez la cara de eztupida?");
+    
+
+}
+
+console.log(typeof(opcion));
+
+/* 
+ToDo:
+
+- Ejercicio de Signos Zodiacales dla sesión Diseño de Algoritmos y solución de problemas"
+
+-Ejercicio del menu para dibujar las 4 figuras geométricas, añadiendo opciones para calcular area y perimetro de cada figura.
+(cuadro, circulo, rectangulo, triangulo)
+
+!-- Instrucciones para estas tareas -- 
+
+Crear los ejercicios en una carpeta de la sesion llamada control de flujo
+
+El repositorio se llama Control de FLujo.
+La informacion debe ser ingresada por el usuario, y los resultados mostrados en consola.
+En Canvas se va subir el link del repositorio.
+El repositorio se sube el MIERCOLES en la sesion 2 de Git y GitHub
+
+Antes del miercoles.
+*/ 
+
